79b36717f192c2fa1774c41e4e7e6bc22bcfc7b0
vilsonmoro <vilsonmoro@gmail.com>
2021-01-17 11:12:48 -0300

    Database seeding

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+insert into TB_CATEGORY (name) values('Books');
+insert into TB_CATEGORY (name) values('Electronis');
+insert into TB_CATEGORY (name) values('Computers');
+insert into TB_CATEGORY (name) values('Foods');
