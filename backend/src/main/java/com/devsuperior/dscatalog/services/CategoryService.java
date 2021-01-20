d5e9cb5a9ed335286b85123da6e771d5ce155eca
vilsonmoro <vilsonmoro@gmail.com>
2021-01-19 21:39:33 -0300

    Post Category

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -38,10 +38,17 @@ public class CategoryService  {
    @Transactional(readOnly = true)
    public CategoryDTO findById(Long id) {
 	  Optional<Category> obj = repository.findById(id);
-	  //trata exceção quando não encontrado
+	  //lança exceção quando objeto não encontrado
 	  Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
 			  
 	  return new CategoryDTO(entity);
    }
+
+public CategoryDTO insert(CategoryDTO dto) {
+	Category entity = new Category();
+	entity.setName(dto.getName());
+	entity = repository.save(entity);
+	return new CategoryDTO(entity);
+}
 }
  
