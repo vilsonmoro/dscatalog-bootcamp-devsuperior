5faa3dfb2f065e41a1fa50c2271ebeebe9f0f60b
vilsonmoro <vilsonmoro@gmail.com>
2021-02-01 21:28:48 -0300

    Loaders e refatoração de imports

--- front-web/src/pages/Catalog/components/ProductDetails/index.tsx
+++ front-web/src/pages/Catalog/components/ProductDetails/index.tsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
-import { ReactComponent as ArrowIcon } from '../../../../core/assets/images/arrow.svg';
+import { ReactComponent as ArrowIcon } from 'core/assets/images/arrow.svg';
 
-import { Product } from '../../../../core/types/Product';
-import { makeRequest } from '../../../../core/utils/request';
+import { Product } from 'core/types/Product';
+import { makeRequest } from 'core/utils/request';
+import ProductDescriptionLoader from '../Loaders/ProductDescriptionLoader';
+import ProductInfoLoader from '../Loaders/ProductInfoLoader';
 import ProductPrice from '../ProductPrice';
 import './styles.scss';
 
@@ -14,12 +16,18 @@ type ParamsType = {
 const ProductDetails = () => {
     const { productId } = useParams<ParamsType>();
     const [product, setProduct] = useState<Product>();
-    
-    useEffect(() => {
-        makeRequest({url:`/products/${productId}`})
-        .then( response => setProduct(response.data));
+    const [isLoading, setIsLoading] = useState(false);
 
-      }, [productId]);
+    useEffect(() => {
+        setIsLoading(true);
+        makeRequest({ url: `/products/${productId}` })
+            .then(response => setProduct(response.data))
+            .finally(() => {
+                //finaliza loader
+                setIsLoading(false);
+            });
+
+    }, [productId]);
 
     return (
         <div className="product-details-container">
@@ -30,22 +38,32 @@ const ProductDetails = () => {
                 </Link>
 
                 <div className="row">
-                    <div className="col-6 pr-5">
-                         <div className="product-details-card text-center">
-                             <img src={product?.imgUrl} alt={product?.name} className="product-details-image" />
-                         </div>
-                         <h1 className="products-details-name">
-                             {product?.name}
-                         </h1>
-                         {product?.price && <ProductPrice price={product?.price} /> }                        
-                    </div>
-                    <div className="col-6 product-details-card">
-                      <h1 className="product-description-title">Descrição do produto</h1>
-                      <p className="product-description-text">
-                         {product?.description}  
-                      </p>
-                    </div>
-                </div>                
+
+                    {isLoading ? <ProductInfoLoader /> : (
+                        <>
+                            <div className="col-6 pr-5">
+                                <div className="product-details-card text-center">
+                                    <img src={product?.imgUrl} alt={product?.name} className="product-details-image" />
+                                </div>
+                                <h1 className="products-details-name">
+                                    {product?.name}
+                                </h1>
+                                {product?.price && <ProductPrice price={product?.price} />}
+                            </div>
+                        </>
+                    )}
+
+                    {isLoading ? <ProductDescriptionLoader /> : (
+                        <>
+                            <div className="col-6 product-details-card">
+                                <h1 className="product-description-title">Descrição do produto</h1>
+                                <p className="product-description-text">
+                                    {product?.description}
+                                </p>
+                            </div>
+                        </>
+                    )}
+                </div>
             </div>
 
         </div>
--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { ProductsResponse } from '../../core/types/Product';
-import { makeRequest } from '../../core/utils/request';
+import { ProductsResponse } from 'core/types/Product';
+import { makeRequest } from 'core/utils/request';
 
 import ProductCard from './components/ProductCard';
-import ProductCardLoader from './components/ProductCardLoader';
+import ProductCardLoader from './components/Loaders/ProductCardLoader';
 import './styles.scss';
 
 const Catalog = () => {
