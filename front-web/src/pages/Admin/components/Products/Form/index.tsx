ca5f27197b6009b87392ea5096eb6a5e58661074
vilsonmoro <vilsonmoro@gmail.com>
2021-02-05 14:48:30 -0300

    Add base form

---
+++ front-web/src/pages/Admin/components/Products/Form/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import BaseForm from '../../BaseForm';
+
+const Form = () => {
+    return (
+        <BaseForm title="CADASTRAR UM PRODUTO">
+            <div className="row">
+                <div className="col-6">
+                    <input type="text" />
+                </div>
+            </div>
+        </BaseForm>
+    );
+}
+
+export default Form;
