4b5812ed2ee046d3d6e2227e129af9f88e0238c7
vilsonmoro <vilsonmoro@gmail.com>
2021-03-01 19:32:41 -0300

    First Test repository

--- backend/src/main/java/com/devsuperior/dscatalog/repositories/ProductRepository.java
+++ backend/src/main/java/com/devsuperior/dscatalog/repositories/ProductRepository.java
@@ -15,6 +15,6 @@ import com.devsuperior.dscatalog.entities.Product;
 public interface ProductRepository extends JpaRepository<Product, Long> {
     @Query("SELECT DISTINCT obj FROM Product obj INNER JOIN obj.categories cats WHERE "
     		+ "(COALESCE(:categories) IS NULL OR cats IN :categories) AND "
-    		+ "(:name = '' OR LOWER(obj.name) LIKE LOWER(CONCAT('%',:name,'%')) ) ")
+    		+ "(LOWER(obj.name) LIKE LOWER(CONCAT('%',:name,'%')) ) ")
 	Page<Product> find(List<Category> categories, String name, Pageable pageable);
 }
---
+++ backend/src/test/java/com/devsuperior/dscatalog/tests/repositories/ProductRepositoryTests.java
@@ -0,0 +1,24 @@
+package com.devsuperior.dscatalog.tests.repositories;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import com.devsuperior.dscatalog.entities.Product;
+import com.devsuperior.dscatalog.repositories.ProductRepository;
+
+@DataJpaTest
+public class ProductRepositoryTests {
+   @Autowired
+   private ProductRepository repository;
+	
+   @Test
+   public void deleteShouldDeleteObjectWhenIdExists() {
+	   repository.deleteById(1L);
+	   Optional<Product> result = repository.findById(1L);
+	   Assertions.assertFalse(result.isPresent());
+   }
+}
