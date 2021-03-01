1fe9bb4570a61ae4b0c8e43c7e07c4a2d71a0390
vilsonmoro <vilsonmoro@gmail.com>
2021-03-01 19:38:37 -0300

    First Test repository with fixtures

--- backend/src/test/java/com/devsuperior/dscatalog/tests/repositories/ProductRepositoryTests.java
+++ backend/src/test/java/com/devsuperior/dscatalog/tests/repositories/ProductRepositoryTests.java
@@ -3,6 +3,7 @@ package com.devsuperior.dscatalog.tests.repositories;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -14,11 +15,18 @@ import com.devsuperior.dscatalog.repositories.ProductRepository;
 public class ProductRepositoryTests {
    @Autowired
    private ProductRepository repository;
+   
+   private long existingId;
+   
+   @BeforeEach
+   void setUp() throws Exception{
+	   existingId = 1L;
+   }
 	
    @Test
    public void deleteShouldDeleteObjectWhenIdExists() {
-	   repository.deleteById(1L);
-	   Optional<Product> result = repository.findById(1L);
+	   repository.deleteById(existingId);
+	   Optional<Product> result = repository.findById(existingId);
 	   Assertions.assertFalse(result.isPresent());
    }
 }
