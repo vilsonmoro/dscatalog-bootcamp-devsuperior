3b6a19119bec2634c69bb706d70b58cf729fb0db
vilsonmoro <vilsonmoro@gmail.com>
2021-03-09 17:24:38 -0300

    Definição de rotas de autenticação

--- front-web/src/Routes.tsx
+++ front-web/src/Routes.tsx
@@ -25,6 +25,8 @@ const Routes = () => (
                  <ProductDetails />
              </Route>
 
+             
+             <Redirect from="/admin/auth" to="/admin/auth/login" exact/>
              <Route path="/admin/auth">
                  <Auth />
              </Route>
