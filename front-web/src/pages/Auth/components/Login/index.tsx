18ce028c952a196484741519ea1073d18484e85d
vilsonmoro <vilsonmoro@gmail.com>
2021-03-10 10:44:05 -0300

    Integrated form with library hook

--- front-web/src/pages/Auth/components/Login/index.tsx
+++ front-web/src/pages/Auth/components/Login/index.tsx
@@ -1,22 +1,39 @@
-import ButtonIcon from 'core/components/ButtonIcon';
 import React from 'react';
+import ButtonIcon from 'core/components/ButtonIcon';
 import { Link } from 'react-router-dom';
+import { useForm } from "react-hook-form";
 import AuthCard from '../Card';
 import './styles.scss';
 
+type FormData = {
+    email: string;
+    password: string;
+}
+
 const Login = () => {
+    const { register, handleSubmit } = useForm<FormData>();
+    
+    const onSubmit = (data: FormData) => {
+        //chama a API
+        console.log(data);
+    }
+
     return (
         <AuthCard title="Login">
-            <form className="login-form">
+            <form className="login-form" onSubmit={handleSubmit(onSubmit)}>
                 <input 
                      type="email" 
                      className="form-control input-base margin-bottom-30"
                      placeholder="Email"
+                     name="email"
+                     ref={register}
                 />
                 <input 
                      type="password" 
                      className="form-control input-base"
                      placeholder="Senha"
+                     name="password"
+                     ref={register}
                 />
                 <Link to="/admin/auth/recover" className="login-link-recover">
                     Esqueci a senha?
