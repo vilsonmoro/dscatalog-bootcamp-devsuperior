22727da91ae2d0e626c21d79bd80f8d01df3be9c
vilsonmoro <vilsonmoro@gmail.com>
2021-03-18 20:46:13 -0300

    Add Card product-admin

---
+++ front-web/src/pages/Admin/components/Products/Card/index.tsx
@@ -0,0 +1,42 @@
+import ProductPrice from 'pages/Catalog/components/ProductPrice';
+import React from 'react';
+import './styles.scss';
+
+const Card = () => {
+    return (
+        <div className="card-base product-card-admin">
+            <div className="row">
+                <div className="col-2 text-center border-right py-3">
+                    <img
+                        src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/22-big.jpg"
+                        alt="Produto teste"
+                        className="product-card-image-admin" />
+                </div>
+                <div className="col-7 py-3">
+                    <h3 className="product-card-name-admin">Computador Desktop - Intel Core i7</h3>
+                    <ProductPrice price={40.50} />
+                    <div>
+                        <span className="badge badge-secondary mr-2">Category 1</span>
+                        <span className="badge badge-secondary mr-2">Category 2</span>
+                        <span className="badge badge-secondary mr-2">Category 3</span>
+                        <span className="badge badge-secondary mr-2">Category 4</span>
+                    </div>
+                </div>
+                <div className="col-3 pt-3 pr-5">
+                    <button
+                        type="button"
+                        className="btn btn-outline-secondary btn-block border-radius-10 mb-3 btn-edit">                    
+                        EDITAR
+                    </button>
+                    <button 
+                       type="button" 
+                       className="btn btn-outline-danger btn-block border-radius-10 mb-3">
+                           EXCLUIR
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Card;
--- front-web/src/pages/Admin/components/Products/List/index.tsx
+++ front-web/src/pages/Admin/components/Products/List/index.tsx
@@ -1,20 +1,26 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
- 
-const List = () =>{
+import Card from '../Card';
+
+const List = () => {
     const history = useHistory();
 
     const handleCreate = () => {
         history.push('/admin/products/create');
     }
 
-    return(
-       <div className="admin-products-list">
-           <button className="btn btn-primary btn-lg" onClick={handleCreate} >
-               ADICIONAR
+    return (
+        <div className="admin-products-list">
+            <button className="btn btn-primary btn-lg" onClick={handleCreate} >
+                ADICIONAR
            </button>
-       </div>
+            <div className="admin-list-container">
+               <Card />
+               <Card />
+               <Card />
+            </div>
+        </div>
     );
 }
- 
+
 export default List;
