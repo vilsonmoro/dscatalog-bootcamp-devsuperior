93f9bcf5c23b2679d9fd29029e96cb3eaa8e6de8
vilsonmoro <vilsonmoro@gmail.com>
2021-03-19 14:37:42 -0300

    Integrating list product with backend

--- front-web/src/pages/Admin/components/Products/Card/index.tsx
+++ front-web/src/pages/Admin/components/Products/Card/index.tsx
@@ -1,37 +1,48 @@
+import { Product, ProductsResponse } from 'core/types/Product';
+import { makeRequest } from 'core/utils/request';
 import ProductPrice from 'pages/Catalog/components/ProductPrice';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './styles.scss';
 
-const Card = () => {
+type Props = {
+    product: Product;
+}
+
+const Card = ({ product }: Props) => {
+
     return (
         <div className="card-base product-card-admin">
             <div className="row">
                 <div className="col-2 text-center border-right py-3">
                     <img
-                        src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/22-big.jpg"
-                        alt="Produto teste"
+                        src={product.imgUrl}
+                        alt={product.name}
                         className="product-card-image-admin" />
                 </div>
                 <div className="col-7 py-3">
-                    <h3 className="product-card-name-admin">Computador Desktop - Intel Core i7</h3>
-                    <ProductPrice price={40.50} />
+                    <h3 className="product-card-name-admin">{product.name}</h3>
+                    <ProductPrice price={product.price} />
                     <div>
-                        <span className="badge badge-secondary mr-2">Category 1</span>
-                        <span className="badge badge-secondary mr-2">Category 2</span>
-                        <span className="badge badge-secondary mr-2">Category 3</span>
-                        <span className="badge badge-secondary mr-2">Category 4</span>
+                        {/*product.categories.map(category => (
+                            <span className="badge badge-secondary mr-2">
+                                {category}
+                            </span>
+                        ))*/}
+                        <span className="badge badge-secondary mr-2"> category </span>
+                        <span className="badge badge-secondary mr-2"> category </span>
+                        <span className="badge badge-secondary mr-2"> category </span>
                     </div>
                 </div>
                 <div className="col-3 pt-3 pr-5">
                     <button
                         type="button"
-                        className="btn btn-outline-secondary btn-block border-radius-10 mb-3 btn-edit">                    
+                        className="btn btn-outline-secondary btn-block border-radius-10 mb-3 btn-edit">
                         EDITAR
                     </button>
-                    <button 
-                       type="button" 
-                       className="btn btn-outline-danger btn-block border-radius-10 mb-3">
-                           EXCLUIR
+                    <button
+                        type="button"
+                        className="btn btn-outline-danger btn-block border-radius-10 mb-3">
+                        EXCLUIR
                     </button>
                 </div>
             </div>
--- front-web/src/pages/Admin/components/Products/List/index.tsx
+++ front-web/src/pages/Admin/components/Products/List/index.tsx
@@ -1,8 +1,31 @@
-import React from 'react';
+import Pagination from 'core/components/Pagination';
+import { ProductsResponse } from 'core/types/Product';
+import { makeRequest } from 'core/utils/request';
+import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import Card from '../Card';
 
 const List = () => {
+    const [productsResponse, setProductsResponse] = useState<ProductsResponse>();
+    const [isLoading, setIsLoading] = useState(false);
+    const [activePage, setActivePage] = useState(0);
+
+    useEffect(() => {
+        const params = {
+            page: activePage,
+            linesPerPage: 4
+        }
+        //inciar loader
+        setIsLoading(true);
+        //fazer requisição
+        makeRequest({ url: '/products', params })
+            .then(response => setProductsResponse(response.data))
+            .finally(() => {
+                //finaliza loader
+                setIsLoading(false);
+            });
+    }, [activePage]);
+
     const history = useHistory();
 
     const handleCreate = () => {
@@ -15,10 +38,16 @@ const List = () => {
                 ADICIONAR
            </button>
             <div className="admin-list-container">
-               <Card />
-               <Card />
-               <Card />
+                {productsResponse?.content.map(product => (
+                    <Card product={product} key={product.id} />
+                ))}
             </div>
+            {productsResponse &&
+                <Pagination
+                    totalPages={productsResponse.totalPages}
+                    activePage={activePage}
+                    onChange={page => setActivePage(page)}
+                />}
         </div>
     );
 }
