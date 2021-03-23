7a6f33f9327c0b5bf90677e0e34234e27ff434b2
vilsonmoro <vilsonmoro@gmail.com>
2021-03-23 11:54:01 -0300

    Add progress bar to component ImageUpload

--- front-web/src/pages/Admin/components/Products/Form/ImageUpload/index.tsx
+++ front-web/src/pages/Admin/components/Products/Form/ImageUpload/index.tsx
@@ -18,8 +18,13 @@ const ImageUpload = () => {
                <small className="upload-text-helper text-primary">A imagem deve ser  JPG ou PNG e não deve ultrapassar
                <strong> 5 mb</strong> .</small>
             </div>
-            <div className="col-6"> 
+            <div className="col-6 upload-placeholder"> 
                <UploadPlaceHolder />
+               <div className="upload-progress-container">
+                   <div className="upload-progress">
+
+                   </div>
+               </div>
             </div>
 
         </div>
